54793692d0116e0a11ef22223c8aabecc5815172
Nickolay Grekalo <kolagrekalo@gmail.com>
2021-01-02 20:01:26 +0200

    created sceleton of our chat

---
+++ resources/js/Pages/Chat/inputMessage.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>Input Message</div>
+</template>
+
+<script>
+export default {
+    name: "inputMessage",
+    props: ["room"],
+}
+</script>
+
+<style scoped>
+
+</style>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ChatController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +21,11 @@ Route::get('/', function () {
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return Inertia\Inertia::render('Dashboard');
 })->name('dashboard');
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/chat', function () {
+    return Inertia\Inertia::render('Chat/container');
+})->name('chat');
+
+Route::middleware("auth:sanctum")->get("chat/rooms", [ChatController::class, "rooms"]);
+Route::middleware("auth:sanctum")->get("chat/room/{roomId}/messages", [ChatController::class, "messages"]);
+Route::middleware("auth:sanctum")->post("chat/room/{roomId}/message", [ChatController::class, "newMessage"]);
